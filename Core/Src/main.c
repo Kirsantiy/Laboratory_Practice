467ef8515e86cf069e74593ed8161450ecbaaea7
Kirsantiy <kirsantiy.gritsenko@mail.ru>
2025-11-04 21:44:05 +0700

    LED PC13 через define

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,3 +1,12 @@
-int main(void){
-    while(1){}
+#include "init.h"
+
+int main(void)
+{
+    GPIO_Init(); 
+
+    while (1)
+    {
+        // ВКЛЮЧЕНИЕ СВЕТОДИОДА (PC13 = LOW)
+        *(uint32_t*)(GPIOC_BASE + GPIOC_BSRR_OFFSET) = PC13_RESET; 
+    }
 }
---
+++ build_F411CE/init.d
@@ -0,0 +1,2 @@
+build_F411CE/init.o: Core/Src/init.c Core/Inc/init.h
+Core/Inc/init.h:
